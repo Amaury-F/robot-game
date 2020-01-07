b28bdafc740f7a545dc2077b8d01a6f7600af7b1
Amaury <amaury.fauvel@gmail.com>
2020-01-07 03:05:11 +0100

    player animations

--- Assets/Scripts/PlayerControl/PlayerControl.cs
+++ Assets/Scripts/PlayerControl/PlayerControl.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 // ReSharper disable CheckNamespace
 
@@ -16,14 +17,20 @@ public class PlayerControl : MonoBehaviour {
     private Vector3 velocity;
     private float velocityXSmoothing;
 
+    private Animator animator;
+    private SpriteRenderer renderer;
+
     private Controller2D controller;
+    private static readonly int WalkDir = Animator.StringToHash("walkDir");
 
     void Start() {
-        controller = GetComponent<Controller2D> ();
+        controller = GetComponent<Controller2D>();
+        animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
 
         gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
         jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-        print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+        print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
     }
 
     void Update() {
@@ -32,14 +39,26 @@ public class PlayerControl : MonoBehaviour {
             velocity.y = 0;
         }
 
-        Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
             velocity.y = jumpVelocity;
+            animator.SetBool("isJumping", true);
+        } else if (controller.collisions.below) {
+            animator.SetBool("isJumping", false);
         }
 
         float targetVelocityX = input.x * moveSpeed;
-        velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+        animator.SetInteger(WalkDir, Math.Sign(targetVelocityX));
+        if (Math.Sign(targetVelocityX) < 0) {
+            renderer.flipX = true;
+        } else if (Math.Sign(targetVelocityX) > 0) {
+            renderer.flipX = false;
+        }
+        
+        
+        velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing,
+            (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
         velocity.y += gravity * Time.deltaTime;
         controller.Move (velocity * Time.deltaTime);
     }
